d881f9ac0ba240d39b132f529f140fa95d176f6f
mistergahan <dkkarpov@gmail.com>
2024-05-28 10:18:37 +0300

    Добавил новые тесты

--- Chantal/ChantalUITests/ChantalUITests.swift
+++ Chantal/ChantalUITests/ChantalUITests.swift
@@ -22,15 +22,95 @@ final class ChantalUITests: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
+    
+    func createTask() throws {
+        let app = XCUIApplication()
+        app.launch()
+        
+        app.navigationBars.buttons["Add"].tap()
+        
+        app.alerts.textFields["Enter task name..."].tap()
+        app.alerts.textFields["Enter task name..."].typeText("Задача")
+        
+        app.alerts.scrollViews.otherElements.buttons["Add"].tap()
+    }
+    
 
     func testExample() throws {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
+        
+        app.navigationBars.buttons["Add"].tap()
+        
+        app.alerts.textFields["Enter task name..."].tap()
+        app.alerts.textFields["Enter task name..."].typeText("Задача")
+        
+        app.alerts.scrollViews.otherElements.buttons["Add"].tap()
+        
+        XCTAssert(app.cells.staticTexts["Задача"].exists)
+        
 
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
+    func testFinishTask() throws {
+        
+        let app = XCUIApplication()
+        app.launch()
+        
+        try createTask()
+        
+        XCTAssert(app.cells.staticTexts["Задача"].exists)
+        
+        app.tables.staticTexts["Задача"].swipeLeft()
+        
+        XCTAssert(app.cells.staticTexts["Задача"].exists)
+    }
+    
+    func testDeletePlannedTask() throws {
+        
+        let app = XCUIApplication()
+        app.launch()
+        
+        try createTask()
+        
+        XCTAssert(app.cells.staticTexts["Задача"].exists)
+    
+        app.tables.staticTexts["Задача"].swipeLeft()
+        app.tables.buttons["delete"].tap()
+        
+        XCTAssertFalse(app.cells.staticTexts["Задача"].exists)
+    }
+    
+    func testDeleteFinishedTask() throws {
+        
+        let app = XCUIApplication()
+        app.launch()
+        
+        try createTask()
+        
+        app.tables.staticTexts["Задача"].swipeRight()
+        app.tables.buttons["done"].tap()
+        app.tables.staticTexts["Задача"].swipeLeft()
+        app.tables.buttons["delete"].tap()
+    
+                                        
+        XCTAssertFalse(app.cells.staticTexts["Задача"].exists)
+        
+    }
+    
+    func testMarkTaskDone() throws {
+        
+        let app = XCUIApplication()
+        app.launch()
+        
+        try createTask()
+        
+        app.tables.staticTexts["Задача"].swipeRight()
+        app.tables.buttons["done"].tap()
+    }
+    
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
             // This measures how long it takes to launch your application.
